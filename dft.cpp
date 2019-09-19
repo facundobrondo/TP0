58a91fccf0080dbd3fe9edc9f72907217e2dc01f
Julian Garcia <juli.garcia.77@gmail.com>
2019-09-19 18:17:14 -0300

    Revisar precision de dft e idft

--- dft.cpp
+++ dft.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 Array<Complex> & dft(Array<Complex> &);
+Array<Complex> & idft(Array<Complex> &);
 Complex Wn(size_t n, size_t k, size_t N, bool positive = true);
 
 int main(void){
@@ -26,6 +27,10 @@ int main(void){
 	Array<Complex> &out = dft(in);
 
 	cout << setprecision(3) << "Vector de salida: " << out << endl;
+
+	Array<Complex> &iout = idft(out);
+
+	cout << setprecision(3) << "Vector de salida idft: " << iout << endl;
 
 	inFile.close();
 
@@ -61,4 +66,26 @@ Complex Wn(size_t n, size_t k, size_t N, bool positive){
 
 	return Complex(cos(2 * PI * n * k / N), sin(2 * PI * n * k / N));
 
+}
+
+Array<Complex> & idft(Array<Complex> & in){
+
+	Array<Complex> *ptr = new Array<Complex>;
+	Array<Complex> &out = *ptr;
+
+	Complex result;
+
+	for(size_t n = 0; n < in.getSize(); n++){
+
+		result = 0;
+
+		for(size_t k = 0; k < in.getSize(); k++)
+			result += in[k] * Wn(n, k, in.getSize(), false);
+
+		result /= in.getSize();
+		out += result;
+
+	}
+
+	return out;
 }
